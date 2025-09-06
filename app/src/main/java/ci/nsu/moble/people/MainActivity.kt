99b3e9e788cb4a7eb6681aa5154ae9bf7c970cb3
konevdi <konevdi@mer.ci.nsu.ru>
2025-09-06 11:09:59 +0700

    Message

--- app/src/main/java/ci/nsu/moble/people/MainActivity.kt
+++ app/src/main/java/ci/nsu/moble/people/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PeopleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "KOMAR",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PeopleTheme {
-        Greeting("Android")
+        Greeting("KOMAR")
     }
 }
